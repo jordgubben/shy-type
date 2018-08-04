c92f151cb1be1af80f2b07a68ca325834d0cb8e4
Jakob Eklund <jakob.eklund@superattack.se>
2018-08-04 11:47:59 +0200

    Migrate code from (closed source) origin project
    Copy code used in several game prototypes/jams into this repo.
    Clean of unrelated cruft. Add package.json (with mocha and chai).

---
+++ src/shy-type.js
@@ -0,0 +1,147 @@
+(function (global, factory){
+	// https://github.com/umdjs/umd/blob/master/templates/returnExports.js 
+	if (typeof define == 'function' && define.amd ) {
+		define([], factory);
+	} else if (typeof module == 'object' && module.exports) {
+		module.exports = factory();
+	} else {
+		global.util = factory();
+	}
+})(this, function() {
+	"use strict";
+
+	/**
+	 * Verify that a value is a boolean.
+	 */
+	function bool(value, hint) {
+		if(typeof value == "boolean") {
+			return value;
+		} else {
+			throw new TypeError(
+				(hint ? hint + " - ": "") +
+				"Expected \'" + value + "\' to be a boolean"
+				);
+		}
+	}
+
+	/**
+	 * Verify that a value is a number.
+	 */
+	function num(value, hint) {
+		//Value has type number, but is not NaN
+		if(typeof value == "number" && value * 0 === 0) {
+			return value;
+		} else {
+			throw new TypeError(
+				(hint ? hint + " - " : "") +
+				"Expected \'" + value + "\' to be a number"
+				);
+		}
+	}
+
+	/**
+	 * Verify that a value is an integer.
+	 */
+	function int(value, hint) {
+		if(num(value, hint) == Math.round(value)) {
+			return value;
+		} else {
+			throw new TypeError(
+				(hint ? hint + " - " : "") +
+				"Expected \'" + value + "\' to be an integer"
+				);			
+		}
+	}
+
+	/**
+	 * Verify that a value is a string.
+	 */
+	function str(value, hint) {
+		if(typeof value == "string") {
+			return value;
+		} else {
+			throw new TypeError(
+				(hint ? hint + " - " : "") + 
+				"Expected \'" + value + "\' to be a string"
+				);
+		}
+	}
+
+
+	/**
+	 * Verify that a value is an object.
+	 */
+	function obj(value, hint) {
+		if(typeof value == "object" && value !== null) {
+			return value;
+		} else {
+			throw new TypeError(
+				(hint ? hint + " - " : "") + 
+				"Expected \'" + value + "\' to be an object"
+				);
+		}
+	}
+
+	/**
+	 * Verify that a value is a function.
+	 */
+	function fun(value, hint) {
+		if(typeof value == "function") {
+			return value;
+		} else {
+			throw new TypeError(
+				(hint ? hint + " - ": "") +
+				"Expected \'" + value + "\' to be a function"
+				);
+		}
+	}
+
+	/**
+	 * Is this value an array?
+	 */
+	function isArr(value) {
+		return Object.prototype.toString.call(value) === Object.prototype.toString.call([])
+	}
+
+	/**
+	 * Verify that a value is an array.
+	 */
+	function arr(value, hint) {
+		if(isArr(value)) {
+			return value;
+		} else {
+			throw new TypeError(
+				(hint ? hint + " - " : "") + 
+				"Expected \'" + value + "\' to be an array"
+				);
+		}
+	}
+
+
+	/**
+	 * Verify that a value it the instance of some type (constructor).
+	 */
+	function isA(type) {
+		return function(value, hint){
+			if(value instanceof type) {
+				return value;
+			} else {
+				throw new TypeError(
+					(hint ? hint + " - " : "") + 
+					"Expected \'" + value + "\' to be an instance of \'" + type +"\'");
+			}
+		}
+	}
+
+	return {
+			bool: bool,
+			int: int,
+			num: num,
+			str: str,
+			obj: obj,
+			fun: fun,
+			arr: arr,
+			isA: isA
+		}
+
+});
---
+++ src/shy-type.test.js
@@ -0,0 +1,179 @@
+(function (){
+//Load typeule and assert lib
+var type, expect;
+if (typeof window === 'undefined') {
+	type = require("./shy-type.js");
+	expect = require("chai").expect;
+} else {
+	before(function (done) {
+		require(["shy-type", "chai"], function (_type, _chai) {
+			type = _type; expect = _chai.expect; done();
+		});
+	});
+}
+
+//Tests
+describe("Module 'shy-type'", function () {
+	"use strict";
+
+	it("loads with AMD", function () {
+		expect(type).to.exist;
+	});
+	
+	describe("type.bool()", function () {
+		
+		var bool;
+		before(function () {
+			bool = type.bool;
+		});
+
+		it("returns the value if it is a boolean", function () {	
+			var someValue = true;	
+			expect(bool(someValue), "Returned value").to.equal(someValue);
+		});
+
+		it("throws TypeError if it's not a boolean", function () {
+			expect(function () { bool(1) }, "Passing a number").to.throw(TypeError);
+		});
+
+		it("optionally provides a hint", function () {
+			try {
+				bool(undefined, "The hint");
+			} catch (error) {
+				expect(error.message).to.contain("The hint");
+			}
+		});
+	});
+
+	describe("type.fun()", function () {
+		
+		var fun;
+		before(function () {
+			fun = type.fun;
+		});
+
+		it("returns the value if it is a function", function () {	
+			var someFunction = function doNothing() {};	
+			expect(fun(someFunction), "Returned value").to.equal(someFunction);
+		});
+
+		it("throws TypeError if it's not a function", function () {
+			expect(function () { fun(1) }, "Passing a number").to.throw(TypeError);
+		});
+
+		it("optionally provides a hint", function () {
+			try {
+				fun(undefined, "The hint");
+			} catch (error) {
+				expect(error.message).to.contain("The hint");
+			}
+		});
+	});
+
+	describe("type.obj()", function () {
+		var obj;
+		before(function () {
+			obj = type.obj;
+		});
+
+		it("returns the value if it is an object", function () {	
+			var someObject = {};	
+			expect(obj(someObject), "Returned value").to.equal(someObject);
+		});
+
+		it("throws TypeError if it's not an object", function () {
+			expect(function () { obj(1) }, "Passing a number").to.throw(TypeError);
+		});
+
+		it("throws TypeError if it's null", function () {
+			expect(function () { obj(null) }, "Passing null").to.throw(TypeError);
+		});
+
+		it("optionally provides a hint", function () {
+			try {
+				obj(undefined, "The hint");
+			} catch (error) {
+				expect(error.message).to.contain("The hint");
+			}
+		});
+	});
+
+	describe("type.num()", function () {
+		var num;
+		before(function () {
+			num = type.num;
+		});
+
+		it("optionally provides a hint", function () {
+			try {
+				num(undefined, "The hint");
+			} catch (error) {
+				expect(error.message).to.contain("The hint");
+			}
+		})
+	});
+
+	describe("type.int()", function () {
+		
+		var int;
+		before(function () {
+			int = type.int;
+		});
+
+		it("returns the value if it is an integer", function () {	
+			var value = 10;
+			expect(int(value), "Returned value").to.equal(value);
+		});
+
+		it("throws TypeError if it's not a Number", function () {
+			expect(function () { int("10") }, "Passing a string").to.throw(TypeError);
+		});
+
+		it("throws TypeError if it's not an integer", function () {
+			expect(function () { int(10.2) }, "Passing a a decimal number").to.throw(TypeError);
+		});
+
+		it("optionally provides a hint", function () {
+			try {
+				int(1.2, "The int hint")
+			} catch (error) {
+				expect(error.message).to.contain("The int hint");
+			}
+
+			try {
+				int(undefined, "The num hint")
+			} catch (error) {
+				expect(error.message).to.contain("The num hint");
+			}
+		})
+	});
+
+
+
+	describe("type.arr()", function () {
+		
+		var arr;
+		before(function () {
+			arr = type.arr;
+		});
+
+		it("returns the value if it is an array", function () {	
+			var value = [];
+			expect(arr(value), "Returned value").to.equal(value);
+		});
+
+		it("throws TypeError if it's not an array", function () {
+			expect(function () { arr("Not an array") }, "Passing a string").to.throw(TypeError);
+		});
+
+		it("optionally provides a hint", function () {
+			try {
+				arr(undefined, "The hint");
+			} catch (error) {
+				expect(error.message).to.contain("The hint");
+			}
+		});
+
+	});
+});
+}());
